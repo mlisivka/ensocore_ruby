3dd3832bb833e26f50297e1e59f3738af9398d74
Mykola <mykola.lisivka@gmail.com>
2018-10-07 23:59:04 +0300

    Updated tests

--- test.rb
+++ test.rb
@@ -2,6 +2,7 @@ class Test
   def initialize
     @successful = 0
     @failed = 0
+    @start_time = Time.now
   end
 
   def run(expected, actual)
@@ -14,9 +15,16 @@ class Test
     end
   end
 
+  def self.start
+    test = Test.new
+    yield test
+    test.finish
+  end
+
   def finish
     all = @successful + @failed
     puts "\n#{all} examples, #{@successful} successful, #{@failed} failed"
+    puts "Finished in #{(Time.now.to_f - @start_time.to_f).round(5)} seconds"
   end
 end
 
--- test_cases.rb
+++ test_cases.rb
@@ -2,37 +2,36 @@ require_relative 'test'
 require_relative 'indices'
 require_relative 'array_with_flatten'
 
-test = Test.new
+Test.start do |t|
 
-list = [2, 7, 11, 15]
-indices = Index.new(*list)
-test.run(indices.indices_of(9),  [0, 1])
-test.run(indices.indices_of(13), [0, 2])
-test.run(indices.indices_of(26), [2, 3])
-test.run(indices.indices_of(17), [0, 3])
-test.run(indices.indices_of(22), [1, 3])
+  list = [2, 7, 11, 15]
+  indices = Index.new(*list)
+  t.run(indices.indices_of(9),  [0, 1])
+  t.run(indices.indices_of(13), [0, 2])
+  t.run(indices.indices_of(26), [2, 3])
+  t.run(indices.indices_of(17), [0, 3])
+  t.run(indices.indices_of(22), [1, 3])
 
-list2 = [1, 14, 7, 2, 4, 3]
-indices2 = Index.new(*list2)
-test.run(indices2.indices_of(3),  [0, 3])
-test.run(indices2.indices_of(4),  [0, 5])
-test.run(indices2.indices_of(5),  [0, 4])
-test.run(indices2.indices_of(6),  [3, 4])
-test.run(indices2.indices_of(7),  [4, 5])
-test.run(indices2.indices_of(8),  [0, 2])
-test.run(indices2.indices_of(21), [1, 2])
-test.run(indices2.indices_of(13), [])
-test.run(indices2.indices_of(22), [])
+  list2 = [1, 14, 7, 2, 4, 3]
+  indices2 = Index.new(*list2)
+  t.run(indices2.indices_of(3),  [0, 3])
+  t.run(indices2.indices_of(4),  [0, 5])
+  t.run(indices2.indices_of(5),  [0, 4])
+  t.run(indices2.indices_of(6),  [3, 4])
+  t.run(indices2.indices_of(7),  [4, 5])
+  t.run(indices2.indices_of(8),  [0, 2])
+  t.run(indices2.indices_of(21), [1, 2])
+  t.run(indices2.indices_of(13), [])
+  t.run(indices2.indices_of(22), [])
 
-test.run(ArrayWithFlatten.new([1, 2, 3, 4]).flatten, [1, 2, 3, 4])
-test.run(ArrayWithFlatten.new([1,[2,[3]],[4]]).flatten, [1, 2, 3, 4])
-test.run(ArrayWithFlatten.new([1,[2,[[3]]],[4]]).flatten, [1, 2, 3, 4])
+  t.run(ArrayWithFlatten.new([1, 2, 3, 4]).flatten, [1, 2, 3, 4])
+  t.run(ArrayWithFlatten.new([1,[2,[3]],[4]]).flatten, [1, 2, 3, 4])
+  t.run(ArrayWithFlatten.new([1,[2,[[3]]],[4]]).flatten, [1, 2, 3, 4])
 
-test.finish
-
-puts
-p indices.indices_of(9)
-p indices.indices_of(13)
-p indices.indices_of(26)
-p indices.indices_of(17)
-p indices.indices_of(22)
+  puts
+  p indices.indices_of(9)
+  p indices.indices_of(13)
+  p indices.indices_of(26)
+  p indices.indices_of(17)
+  p indices.indices_of(22)
+end
